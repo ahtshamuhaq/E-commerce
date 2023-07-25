5873cf392f68609a8042aa50d657c3a769e477c7
Ahtsham Ul Haq <112152384+ahtshamuhaq@users.noreply.github.com>
2023-07-25 18:04:24 +0500

    made home screen and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,38 @@
 import React from "react";
-import data from "./Data";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">MR-Commerce</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((products) => (
-            <div className="product" key={products.slug}>
-              <a href={`/products/${products.slug}`}>
-                <img src={products.image} alt={products.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/products/${products.slug}`}>
-                  <p>{products.name}</p>
-                </a>
-                <p>
-                  <strong>${products.price}</strong>
-                </p>
-                <button>Add To Cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container className="">
+              <LinkContainer to="/">
+                <Navbar.Brand>MR-COMMERCE</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center font-weight-bold fs-3 mt-4">
+            2023 @ - Copyright -- All rights reserved - Ahtsham Ulhaq
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
