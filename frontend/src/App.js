2fd2e9b9a2cbfa9108cfed94e59ebc75a73decf8
Ahtsham Ul Haq <112152384+ahtshamuhaq@users.noreply.github.com>
2023-07-25 11:13:14 +0500

    made initial products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
+import React from "react";
+import data from "./Data";
 function App() {
   return (
     <div>
       <header>
-       <a href="/">MR-Commerce</a>
+        <a href="/">MR-Commerce</a>
       </header>
       <main>
-        List Products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((products) => (
+            <div className="product" key={products.slug}>
+              <a href={`/products/${products.slug}`}>
+                <img src={products.image} alt={products.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/products/${products.slug}`}>
+                  <p>{products.name}</p>
+                </a>
+                <p>
+                  <strong>${products.price}</strong>
+                </p>
+                <button>Add To Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
